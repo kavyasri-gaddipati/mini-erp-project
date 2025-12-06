ab974705bfc09fa72a7386e662db7ec6b9f7e3ff
kavyasri-gaddipati <gaddipatikavyasri@gmail.com>
2025-12-06 19:34:44 +0530

    Fixed db.js require error

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,16 @@
+// server/config/db.js
+const mysql = require('mysql2'); // Ee line miss aindi, anduke error vachindi!
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 4000, // TiDB uses port 4000
-    ssl: { rejectUnauthorized: true } // Required for Cloud
+    port: 4000,
+    ssl: { rejectUnauthorized: true }
 });
+
 module.exports = pool.promise();
